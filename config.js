5b18f3e47724b990ebacee76a2c7edffd6e93229
Tptin07 <88793248+Tptin07@users.noreply.github.com>
2021-08-11 09:02:56 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thằng đầu moi!',
+    introDesc: `Nhìn mặt m ngu vl
+    Đã thế còn hãm L
+    T gửi cái ni cho m
+    Để coi t viết code ri được chưa?`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Tao siêu cấp vjp pro 😙',
+    desc: 'Tín đẹp trai phải không ? ',
+    btnYes: 'Thề! Đẹp trai vcl <33',
+    btnNo: 'Đéo, đẹp trai con c :3',
+    question:'Đấy, bấm đẹp trai cho nhanh! <3',
+    btnReply: 'chó <3',
+    reply: 'Đoạn ni kb viết chi hết á! <33333333',
+    mess: 'Ngang rứa 🥰 ',
+    messDesc: 'Test hết rep lại ib nghe.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tran.cris.77
 }
