159cbc22866028b6093d8368c27123ce8a0aa252
Tptin07 <88793248+Tptin07@users.noreply.github.com>
2021-08-12 00:28:25 +0700

    cofig

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Alo!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Ngày ni t test code
+    Nếu code này thành công thì sạch sẹ
+    Kb viết chi hết`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
